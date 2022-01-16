94c951a5d944898236640c92dd0736e9de9b8ee2
ryanjhunter <ryanjhunter@gmail.com>
2022-01-15 18:32:42 -0800

    Add basic header

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGopuram, faWallet } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faGopuram, faWallet);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
